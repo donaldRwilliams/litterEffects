a6068b138ceb81cfc989ff0c7831c959b5a94707
Donald R. Williams <brycewayne03.29@gmail.com>
2017-05-26 07:46:13 -0700

    back to version before r-studio test

--- R/bias_correction.R
+++ R/bias_correction.R
@@ -1,4 +1,4 @@
-#' GEE Bias Correction (donny)d
+#' GEE Bias Correction 
 #'
 #' This function allows the user to determine the "correct" bias correction
 #' for generalized estimating equations. The corrections are to ensure the
