b78b962fd5658e005f6ced2f0bc353f55aace8d7
Donald R. Williams <brycewayne03.29@gmail.com>
2017-05-25 23:07:41 -0700

    Delete bias_correction.R

--- man/bias_correction.R
+++
@@ -1,8 +0,0 @@
-bias_correction <- function(nsim, b_0, b_treat, icc, v_overall, 
-                            n_litters, pups_litter){
-  p_values <-  replicate(nsim, bias_func(b_0, b_treat, 
-                                         icc, v_overall, n_litters, pups_litter)) 
-  results(p_values)
-  
-}
-
