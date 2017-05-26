c1a4fe1e78677db35fb1d3637b222d8ef04c5833
Donald R. Williams <brycewayne03.29@gmail.com>
2017-05-26 07:21:05 -0700

    Update bias_correction.R

--- R/bias_correction.R
+++ R/bias_correction.R
@@ -11,14 +11,45 @@
 #' to educational settings, this would be equivalent to entire schools receiving 
 #' an intervention and the students were the observational units.
 #' @param nsim Number of simulations. Default is set to 500 
+#' @param v_overall  total variance (litter + residual variance)
 #' @param icc  Intra-class correlation coefficient for the litter effect (between 0.05 to 0.95 of the total variance)
 #' @param n_litters Number of litters in the study (or number of schools)
 #' @param pups_litter Number observations per litter (or student from a given school)
 #' @keywords GEE
 #' @export
 #' @details n_litters must be even. Returned value closest to pretermined alpha level should be used as correction 
+#' @references Fay and Graubard (2001). Small-Sample Adjustments for 
+#' Wald-Type Tests Using Sandwich Estimators. Biometrics 57: 1198-1206.  
+#' 
+#' Gunsolley, J. C., Getchell, C., & Chinchilli, V. M. (1995). Small sample characteristics of generalized 
+#' estimating equations. Communications in Statistics-simulation and Computation, 24: 869-878.
+#' 
+#' Williams, D.R., Burkner, P.C., Carlsson, R. (2017). Between-litter variation in developmental studies of hormones and behavior: 
+#' effects on false positives and statistical power. https://osf.io/wydmc/
 #' @examples
 #' bias_correction()
+#' 
+#' # for loop to investigate type I according to litter number
+#' 
+#' # number of litters
+#' n_lit <- seq(5, 20, 5)
+#' 
+#' # matrix to store results
+#' mat <- matrix(ncol = 6, nrow = length(n_lit))
+#'  
+#' # simulaton
+#' 
+#' for(j in 1:length(n_lit)){
+#'       lit <- n_lit[j] 
+#'       mat[j, 1:6] <- bias_correction(nsim = 500, 
+#'       icc = 0.5, v_overall = 10, n_litters = lit , 
+#'       pups_litter = 4)[1:6]
+#'}
+#'    
+#' res_df <- as.data.table(cbind(n_lit, mat))
+#' colnames(res_df) <- c("n_litters", "d1", "d2", 
+#'                        "d3", "d4", "d5", "dm")
+#' res_df
 
 bias_correction <- function(nsim = 500, icc, v_overall, n_litters, pups_litter){ 
                    p_values <-  replicate(nsim, data_func(icc, v_overall, 
